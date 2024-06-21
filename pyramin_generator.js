9d792f1f2f733e5eedd9962c8e9f340ad3d33589
jtamprime3 <jtamprime@gmail.com>
2024-06-21 11:42:35 -0400

    Update pyramin_generator.js
    project start from free code camp

--- pyramin_generator.js
+++ pyramin_generator.js
@@ -1,1 +1,27 @@
+const character = "#";
+const count = 8;
+const rows = [];
 
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+  done++;
+  rows.push(padRow(done, count));
+  if (done === count) {
+    continueLoop = false;
+  } 
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

bda195db35f0f76cabd26b4bf2510c888b0f7b0c
jtamprime3 <jtamprime@gmail.com>
2024-06-21 14:06:05 -0400

    Complete grade.js

--- grade.js
+++ grade.js
@@ -35,3 +35,15 @@ function hasPassingGrade(score) {
 console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
+
+
+function studentMsg(totalScores, studentScore) {
+  if ( studentScore > getAverage(totalScores)){
+    return "Class average: " + getAverage(totalScores) + ". Your grade: "+ getGrade (studentScore) + ". You passed the course."
+  }
+  else {
+    return "Class average: " + getAverage(totalScores) + ". Your grade: "+ getGrade(studentScore) + ". You failed the course."
+  }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
